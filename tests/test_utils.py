80583ebe9f3457cc7a780e3671c30983a98d01b0
fdrgsp <federico.gasparoli@gmail.com>
2023-03-19 17:54:27 -0400

    test: update test_unique_folder

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,18 +4,19 @@ from pymmcore_mda_writers import BaseMDASequenceWriter
 
 
 def test_unique_folder(tmp_path: Path):
-    base_folder = tmp_path / "data/run"
+    base_folder = tmp_path / "data"
+    name = "run"
     unique = BaseMDASequenceWriter.get_unique_folder
     for i in range(3):
         print("here: ", i)
-        print(unique(base_folder, create=True))
+        print(unique(base_folder, name, create=True))
     unique(base_folder)
 
-    data_folders = list(map(str, set(tmp_path.glob("data/run/run_*"))))
+    data_folders = list(map(str, set(tmp_path.glob("data/run_*"))))
     expected = [
         "run_000",
         "run_001",
         "run_002",
     ]
     for e in expected:
-        assert str(tmp_path / "data/run" / e) in data_folders
+        assert str(tmp_path / "data/" / e) in data_folders
